8505ffc53b565f6d98a94eac163eddea17de4d57
Anders <anders.morille@gmail.com>
2023-09-29 17:03:08 +0200

    Moved sprites to assets/sprites

--- src/sprites.rs
+++ src/sprites.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+
 use macroquad::color::Color;
 use macroquad::prelude::{load_texture, Texture2D};
 
@@ -11,7 +12,7 @@ pub struct Sprite {
 #[derive(Debug)]
 pub struct SpriteLoader(HashMap<String, Sprite>);
 
-pub struct PathColor (pub String, pub Color);
+pub struct PathColor(pub String, pub Color);
 
 impl From<(&str, Color)> for PathColor {
     fn from(value: (&str, Color)) -> Self {
@@ -28,15 +29,16 @@ impl SpriteLoader {
     pub async fn load_many<T: Into<PathColor>>(&mut self, paths: Vec<T>) {
         for pc in paths {
             let path_color = pc.into();
-            let full_path = path_color.0.to_string() + ".png";
+            let full_path = format!("assets/sprites/{}.png", path_color.0);
             let texture = load_texture(&full_path).await;
 
-            let t = texture.unwrap_or_else(|_err| panic!("Invalid sprite name argument! Path: {}",
-                                                         full_path));
+            let t = texture.unwrap_or_else(
+                |_err| panic!("Invalid sprite name argument! Path: {}", full_path)
+            );
 
             self.0.insert(path_color.0, Sprite {
                 color: path_color.1,
-                texture: t
+                texture: t,
             });
         }
     }
