bed8e13b15becb900ba731ca5066ace9088dce0c
Jonathan Jensen <jonathan.obsidion@gmail.com>
2023-10-02 18:03:41 +0200

    Expanded `Vector2` implementation.
    - Added `From<(f64, f64)>` implementation.
    - Added detailed documentation for methods and associated functions.

--- src/library/utils/vector.rs
+++ src/library/utils/vector.rs
@@ -12,16 +12,45 @@ impl From<f64> for Vector2 {
         Self(value.cos(), value.sin())
     }
 }
+impl From<(f64, f64)> for Vector2 {
+    fn from(value: (f64, f64)) -> Self {
+        Self(value.0, value.1)
+    }
+}
 
 impl Vector2 {
+    /// The null vector.
+    /// 
+    /// ## Example
+    /// 
+    /// ```
+    /// assert_eq!(Vector2::ZERO, Vector2(0.00, 0.00));
+    /// ```
     pub const ZERO: Vector2 = Vector2(0.00, 0.00);
+    /// Returns the length of the vector *before* applying the square root.
+    /// 
+    /// ## Example
+    /// 
+    /// ```
+    /// let v = Vector2(1.00, 1.00);
+    /// assert_eq!(v.square_len(), 2.00);
+    /// ```
     pub fn square_len(self) -> f64 {
         let Vector2(x, y) = self;
         x.powi(2) + y.powi(2)
     }
+    /// Returns the length of the vector. Equivalent to `self.square_len().sqrt()`.
+    /// 
+    /// ## Example
+    /// 
+    /// ```
+    /// let v = Vector2(1.00, 1.00);
+    /// assert_eq!(v.len(), 2.00f64.sqrt());
+    /// ```
     pub fn len(self) -> f64 {
         self.square_len().sqrt()
     }
+    /// Returns the equivalent vector with a length of 1. Essentially performs `self / self.len()`.
     pub fn normal(self) -> Vector2 {
         let length = self.len();
         let Vector2(x, y) = self;
@@ -31,28 +60,58 @@ impl Vector2 {
             Vector2::ZERO
         }
     }
+    /// Returns the angle of the vector in radians.
+    /// 
+    /// This method uses the `atan2` implementation, and will therefore always respect the signs of the components.
     pub fn angle(self) -> f64 {
         let Vector2(x, y) = self;
         y.atan2(x)
     }
+    /// Returns the negated vector. Equivalent to `self * -1.00`.
     pub fn negate(self) -> Vector2 {
         let Vector2(x, y) = self;
         Vector2(-x, -y)
     }
+    /// Returns the vector with its component values swapped.
     pub fn invert(self) -> Vector2 {
         let Vector2(x, y) = self;
         Vector2(y, x)
     }
+    /// Returns the vector rotated 90 degrees counter-clockwise.
+    /// 
+    /// ## Example
+    /// 
+    /// ```
+    /// let v = Vector2(0.00, 1.00);
+    /// assert_eq!(v.rotate_once(), Vector2(-1.00, 0.00));
+    /// ```
     pub fn rotate_once(self) -> Vector2 {
         let Vector2(x, y) = self;
         Vector2(-y, x)
     }
+    /// Returns the x component of the vector.
+    /// 
+    /// ## Example
+    /// 
+    /// ```
+    /// let v = Vector2(1.00, 2.00);
+    /// assert_eq!(v.x(), 1.00);
+    /// ```
     pub fn x(self) -> f64 {
         self.0
     }
+    /// Returns the y component of the vector.
+    /// 
+    /// ## Example
+    /// 
+    /// ```
+    /// let v = Vector2(1.00, 2.00);
+    /// assert_eq!(v.y(), 2.00);
+    /// ```
     pub fn y(self) -> f64 {
         self.1
     }
+    /// Returns the vector with its x component set to the given value.
     pub fn aligned(self) -> Vector2 {
         let Vector2(x, y) = self;
         if x.abs() > y.abs() {
@@ -61,6 +120,17 @@ impl Vector2 {
             Vector2(0.00, y)
         }
     }
+    /// Returns the vector multiplied component-wise by the given vector.
+    /// 
+    /// Should not be confused with the dot product.
+    /// 
+    /// ## Example
+    /// 
+    /// ```
+    /// let v1 = Vector2(1.00, 2.00);
+    /// let v2 = Vector2(-1.00, 2.00);
+    /// assert_eq!(v1.mul_per(v2), Vector2(-1.00, 4.00));
+    /// ```
     pub fn mul_per(self, rhs: Vector2) -> Vector2 {
         let Vector2(x, y) = self;
         let Vector2(ox, oy) = rhs;
