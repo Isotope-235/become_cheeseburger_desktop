e785be07cd2756003e1d1643d85448eb903a0a5a
Jonathan Jensen <jonathan.obsidion@gmail.com>
2025-12-27 16:07:16 +0100

    Fixed double dash

--- src/main.rs
+++ src/main.rs
@@ -179,19 +179,6 @@ impl State {
                 continue;
             }
 
-            // special update behaviour
-            {
-                // burger
-                let burger = &mut self.burger;
-                burger.vel = input.dir().normal() * (0.55) * dt + burger.vel * 0.675f64.powf(dt);
-                burger.invuln = (burger.invuln - dt).max(0.00);
-                burger.dash_charge = (burger.dash_charge + 0.01 * dt).min(1.00);
-                burger.hp = burger.hp.min(burger.max_hp());
-                if input.space.is_pressed() && burger.can_dash() && input.dir().len() > 0.00 {
-                    burger.dash(input, asset_loader);
-                }
-            };
-
             // up difficulty
             self.difficulty += 0.10 * dt;
         }
