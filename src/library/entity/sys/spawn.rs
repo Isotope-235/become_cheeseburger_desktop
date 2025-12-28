ce7e22d95e5c02ee34da959d36a80c36b242f9b0
Jonathan Jensen <jonathan.obsidion@gmail.com>
2025-12-28 10:05:20 +0100

    Dedup spawn func

--- src/library/entity/sys/spawn.rs
+++ src/library/entity/sys/spawn.rs
@@ -10,10 +10,9 @@ use crate::{
 pub fn run(state: &mut State, dt: f64) {
     let diff_scale = state.difficulty * 0.01;
 
-    // bullets
-    let times = state.counters.bullet.revolve(1.10 + 0.20 * diff_scale, dt);
+    let entities = &mut state.entities;
 
-    for _ in 0..times {
+    state.counters.bullet.run(1.10 + 0.20 * diff_scale, dt, || {
         let side = rrange(4);
         let snake_ch = diff_scale * 0.25;
         if chance(snake_ch / (1.00 + snake_ch)) {
@@ -27,93 +26,84 @@ pub fn run(state: &mut State, dt: f64) {
                     let pos = CENTER + direction.mul_per(CENTER) + buffer;
                     (pos + shift, direction.negate())
                 };
-                spawn(state, Class::Bullet, pos, vel * 1.25, 750. + delay);
+                spawn(entities, Class::Bullet, pos, vel * 1.25, 750. + delay);
             }
         } else {
             for i in 0..((1.00 + diff_scale * 2.00) as i32) {
                 let delay = f64::from(i) * 10.00;
                 let (pos, vel) = spawn_pos_vel_from(side, 4.00 + delay, 4.00);
-                spawn(state, Class::Bullet, pos, vel * 1.25, 750. + delay);
+                spawn(entities, Class::Bullet, pos, vel * 1.25, 750. + delay);
             }
         }
-    }
+    });
 
-    // slugs
-    let times = state.counters.slug.revolve(0.125 + 0.025 * diff_scale, dt);
-
-    for _ in 0..times {
+    state.counters.slug.run(0.125 + 0.025 * diff_scale, dt, || {
         let (pos, vel) = spawn_pos_vel(10.00, 10.00);
-        spawn(state, Class::Slug, pos, vel * 0.50, 1500.);
-    }
+        spawn(entities, Class::Slug, pos, vel * 0.50, 1500.);
+    });
 
-    // warnings
-    let times = state.counters.warning.revolve(0.15 + 0.10 * diff_scale, dt);
+    state
+        .counters
+        .warning
+        .run(0.15 + 0.10 * diff_scale, dt, || {
+            for i in 0..diff_scale as i32 {
+                let (mut pos, dir) = spawn_pos_vel(-12.00, 12.00);
+                // move laser so it targets player
+                let shift = crate::rand(30.00) - 15.00;
+                if dir.x().abs() < 1e-10 {
+                    pos.0 = state.burger.pos.x() + shift;
+                } else {
+                    pos.1 = state.burger.pos.y() + shift;
+                }
+                let delay = f64::from(i) * (15.00);
+                spawn(
+                    entities,
+                    Class::Warning { dir, delay },
+                    pos,
+                    Vector2::ZERO,
+                    60. + delay
+                );
+            }
+        });
 
-    for i in 0..(times * diff_scale as i32) {
-        let (mut pos, dir) = spawn_pos_vel(-12.00, 12.00);
-        // move laser so it targets player
-        let shift = crate::rand(30.00) - 15.00;
-        if dir.x().abs() < 1e-10 {
-            pos.0 = state.burger.pos.x() + shift;
-        } else {
-            pos.1 = state.burger.pos.y() + shift;
-        }
-        let delay = f64::from(i) * (15.00);
-        spawn(
-            state,
-            Class::Warning { dir, delay },
-            pos,
-            Vector2::ZERO,
-            60. + delay
-        );
-    }
-
-    // health packs
-    let hp_count = state
-        .entities
+    let hp_count = entities
         .iter()
         .filter(|e| matches!(e.class, Class::HealthPack))
         .count();
-    let times = state.counters.health_pack.revolve(
+    state.counters.health_pack.run(
         0.10 * f64::from(
             (state.burger.missing_hp() - i32::try_from(hp_count * 2).unwrap()).clamp(0, 8)
         ),
-        dt
+        dt,
+        || {
+            let (pos, vel) = spawn_pos_vel(10.00, 12.00);
+            spawn(entities, Class::HealthPack, pos, vel * 0.30, 500.);
+        }
     );
 
-    for _ in 0..times {
-        let (pos, vel) = spawn_pos_vel(10.00, 12.00);
-        spawn(state, Class::HealthPack, pos, vel * 0.30, 500.);
-    }
-
-    // frag
-    let times = state.counters.frag.revolve(0.10 + 0.02 * diff_scale, dt);
-
-    for _ in 0..times {
+    state.counters.frag.run(0.10 + 0.02 * diff_scale, dt, || {
         let (pos, vel) = spawn_pos_vel(4.00, 4.00);
-        spawn(state, Class::Flak, pos, vel * 0.50, 200.);
-    }
+        spawn(entities, Class::Flak, pos, vel * 0.50, 200.);
+    });
 
-    let times = state
+    state
         .counters
         .cross
-        .revolve((-0.25 + 0.135 * diff_scale).max(0.00), dt);
-
-    for _ in 0..times {
-        for i in 0..4 {
-            let starting_point = SCREEN.mul_per(num_to_corner(i));
-            let direction = CENTER - starting_point;
-            let vel = direction.normal();
-            for ii in 0..3 {
-                let pos = starting_point - vel * 10.00 * f64::from(ii);
-                spawn(state, Class::Bullet, pos, vel * 1.75, 750.);
+        .run((-0.25 + 0.135 * diff_scale).max(0.00), dt, || {
+            for i in 0..4 {
+                let starting_point = SCREEN.mul_per(num_to_corner(i));
+                let direction = CENTER - starting_point;
+                let vel = direction.normal();
+                for ii in 0..3 {
+                    let pos = starting_point - vel * 10.00 * f64::from(ii);
+                    spawn(entities, Class::Bullet, pos, vel * 1.75, 750.);
+                }
             }
-        }
-    }
+        });
 }
 
-fn spawn(state: &mut State, class: Class, pos: Vector2, vel: Vector2, lifespan: f64) {
-    state.entities.push(Entity {
+fn spawn(entities: &mut Vec<Entity>, class: Class, pos: Vector2, vel: Vector2, lifespan: f64) {
+    entities.push(Entity {
         class,
         pos,
         vel,
--- src/library/utils/counter.rs
+++ src/library/utils/counter.rs
@@ -1,5 +1,6 @@
 pub trait Counter {
     fn revolve(&mut self, delta: f64, dt: f64) -> i32;
+    fn run<F: FnMut()>(&mut self, delta: f64, dt: f64, f: F);
 }
 
 impl Counter for f64 {
@@ -8,5 +9,13 @@ impl Counter for f64 {
         let times = *self as i32 / 100;
         *self %= 100.00;
         times
+    }
+
+    fn run<F: FnMut()>(&mut self, delta: f64, dt: f64, mut f: F) {
+        let times = self.revolve(delta, dt);
+
+        for _ in 0..times {
+            f();
+        }
     }
 }
